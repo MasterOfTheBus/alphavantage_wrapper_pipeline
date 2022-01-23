679e793a9a00b900fa476a9008c2f8473b48e635
Sidney Ng <yendisng@gmail.com>
2022-01-23 14:15:37 -0500

    adding dependency on the pipeline lib

--- .projenrc.js
+++ .projenrc.js
@@ -5,9 +5,13 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   name: 'alphavantage_wrapper_pipeline',
 
   // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                    /* Runtime dependencies of this module. */
-  // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                 /* Build dependencies for this module. */
+  deps: [ /* Runtime dependencies of this module. */
+    '@masterofthebus/cdk-pipeline-lib@0.1.18'
+  ],
+  description: 'Creates a pipeline for a service that interacts with AlphaVantage API',
+  devDeps: [ /* Build dependencies for this module. */
+    '@masterofthebus/cdk-pipeline-lib@0.1.18'
+  ],
   // packageName: undefined,      /* The "name" in package.json. */
   // release: undefined,          /* Add release management to this project. */
 });
